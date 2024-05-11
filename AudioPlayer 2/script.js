1dfc1bb9458147dc39a64541da762f037e4b2c74
Alex <aleks.budaghyan@tumo.org>
2024-05-11 09:35:45 +0400

    a variable

--- AudioPlayer 2/script.js
+++ AudioPlayer 2/script.js
@@ -2,7 +2,7 @@ for (let index = 0; index < array.length; index++) {
     const element = array[index];
     for (let index = 0; index < array.length; index++) {
         const element = array[index];
-        
+        var a
     }
     
 }
