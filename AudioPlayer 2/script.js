65fabee4263f544f040b2a7cfcc8e5329f1bfb30
Alex <aleks.budaghyan@tumo.org>
2024-05-11 09:48:54 +0400

    js

--- AudioPlayer 2/script.js
+++
@@ -1,8 +0,0 @@
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    for (let index = 0; index < array.length; index++) {
-        const element = array[index];
-        var a
-    }
-    
-}
